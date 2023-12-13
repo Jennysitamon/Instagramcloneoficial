497df8eb75445f74021ada9021a59cf8a7ad5a32
jeny <jenymontiel13@gmail.com>
2023-12-13 14:35:41 -0600

    InstagramClone de Jenyp2

--- app/src/main/java/com/neatroots/instagramclone/fragments/HomeFragment.kt
+++ app/src/main/java/com/neatroots/instagramclone/fragments/HomeFragment.kt
@@ -34,6 +34,8 @@ class HomeFragment : Fragment() {
 
     }
 
+    ////otro
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
